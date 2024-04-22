c3e578f362e8951068be8c9a6e923390d9215d4c
Jan Allersma <jan@allersma.be>
2024-04-22 12:02:46 +0200

    Add mock and spy

--- src/test/java/com/nhlstenden/jallersma/maventemplate/TestMyTest.java
+++ src/test/java/com/nhlstenden/jallersma/maventemplate/TestMyTest.java
@@ -1,4 +1,23 @@
 package com.nhlstenden.jallersma.maventemplate;
 
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
 public class TestMyTest {
+    @Test
+    public void actionLoggedSpyTest() {
+        User userSpy = spy(new User("Test"));
+        MyMath math = new MyMath(userSpy);
+        math.power(3);
+        verify(userSpy, times(1)).log("power calculation");
+    }
+
+    @Test
+    public void actionLoggedMockTest() {
+        User userMock = mock();
+        MyMath math = new MyMath(userMock);
+        math.power(3);
+        verify(userMock, times(1)).log("power calculation");
+    }
 }
